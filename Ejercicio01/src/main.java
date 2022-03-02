002f406e39d4327f154b468362e8475c05f0e33c
QuiqueTortosa <49041109+QuiqueTortosa@users.noreply.github.com>
2022-03-02 15:32:35 +0100

    main comprobacion clase resta

--- Ejercicio01/src/main.java
+++ Ejercicio01/src/main.java
@@ -6,7 +6,8 @@ public class main {
         Scanner sc = new Scanner(System.in);
 
         Suma su = new Suma(0);
-        int opcion, n1, n2, suma, sumAc;
+        Resta r = new Resta(0);
+        int opcion, n1, n2, suma, sumAc, resta, restAc;
         boolean flag = true;
         while (flag) {
             System.out.println("Escoja una de las dos opciones \n1: Suma \n2: Resta \n3: Finalizar programa");
@@ -34,7 +35,27 @@ public class main {
                     System.out.println("Eleccion incorrecta");
                 }
             } else if (opcion == 2) {
-
+                int restaTipo;
+                System.out.println("Escoja una de las dos opciones \n1: Resta \n2: Resta Acumulada");
+                restaTipo = sc.nextInt();
+                if (restaTipo == 1) {
+                    System.out.println("Introduzca n1");
+                    n1 = sc.nextInt();
+                    System.out.println("Introduzca n2");
+                    n2 = sc.nextInt();
+                    resta = Resta.restar(n1, n2);
+                    System.out.println("Resta: " + resta);
+                } else if (restaTipo == 2) {
+                    System.out.println("Resta acumulada:");
+                    System.out.println("Introduzca un nm:");
+                    restAc = sc.nextInt();
+                    System.out.println("El resultado es:" + r.restA(restAc));
+                    System.out.println("Introduzca otro nm:");
+                    restAc = sc.nextInt();
+                    System.out.println("El resultado es:" + r.restA(restAc));
+                } else {
+                    System.out.println("Eleccion incorrecta");
+                }
             } else {
                 System.out.println("Programa finalizado");
                 flag = false;
