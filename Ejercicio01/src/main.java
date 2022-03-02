014f185024046973270c409eb9a0db98017797f5
QuiqueTortosa <49041109+QuiqueTortosa@users.noreply.github.com>
2022-03-02 15:25:02 +0100

    main comprobacion clase suma

--- Ejercicio01/src/main.java
+++ Ejercicio01/src/main.java
@@ -3,7 +3,43 @@ import java.util.Scanner;
 public class main {
     // Main driver method
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        Suma su = new Suma(0);
+        int opcion, n1, n2, suma, sumAc;
+        boolean flag = true;
+        while (flag) {
+            System.out.println("Escoja una de las dos opciones \n1: Suma \n2: Resta \n3: Finalizar programa");
+            opcion = sc.nextInt();
+            if (opcion == 1) {
+                int sumaTipo;
+                System.out.println("Escoja una de las dos opciones \n1: Suma \n2: Suma Acumulada");
+                sumaTipo = sc.nextInt();
+                if (sumaTipo == 1) {
+                    System.out.println("Introduzca n1");
+                    n1 = sc.nextInt();
+                    System.out.println("Introduzca n2");
+                    n2 = sc.nextInt();
+                    suma = Suma.sumar(n1, n2);
+                    System.out.println("Suma: " + suma);
+                } else if (sumaTipo == 2) {
+                    System.out.println("Suma acumulada:");
+                    System.out.println("Introduzca un nm:");
+                    sumAc = sc.nextInt();
+                    System.out.println("El resultado es:" + su.sumA(sumAc));
+                    System.out.println("Introduzca otro nm:");
+                    sumAc = sc.nextInt();
+                    System.out.println("El resultado es:" + su.sumA(sumAc));
+                } else {
+                    System.out.println("Eleccion incorrecta");
+                }
+            } else if (opcion == 2) {
+
+            } else {
+                System.out.println("Programa finalizado");
+                flag = false;
+            }
+        }
 
 
     }
